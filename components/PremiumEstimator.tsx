32f3bc686bb105696a0b68f0022b79654ae8f330
Jake Edwards <jake.edwards@simplybusiness.com>
2024-06-05 14:59:09 -0400

    initial project

---
+++ components/PremiumEstimator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PremiumEstimator = () => {
+  return (
+    <section>
+      <header className="home-header items-center">Premium Estimator</header>
+    </section>
+  );
+};
+
+export default PremiumEstimator;
