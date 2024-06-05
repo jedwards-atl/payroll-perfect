ed8c752a9c4d2958df1bcb05ccba555b1ea3ac87
Matt Kalin <matt.kalin@simplybusiness.com>
2024-06-05 16:38:09 -0400

    Header

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,27 @@ const Home = () => {
   return (
     <section className="home md:container md:mx-auto">
       <div className="home-content">
-        <header className="home-header">Payroll Perfect</header>
+
+          <div class="coronavirus-cover-lookup">
+              <div class="module form">
+                  <div class="module-content">
+                      <div class="left-content">
+                          <h2>Estimate in seconds</h2>
+                          <h3>Workers' Comp in a click. Simply slide your payroll tool to get an estimate for your workers' comp
+                              policy!</h3>
+                          <ul>
+                              <li class="credit-card">Slide</li>
+                              <li class="file-certificate">Estimate</li>
+                              <li class="message-smile">Insure</li>
+                          </ul>
+                      </div>
+                      <div class="right-content">
+                          <img alt="Woman and man looking at laptop"
+                               src="woman-and-man-looking-at-laptop.png"></img>
+                      </div>
+                  </div>
+              </div>
+          </div>
 
         <div className="flex flex-col items-center">
           <PremiumEstimator />
