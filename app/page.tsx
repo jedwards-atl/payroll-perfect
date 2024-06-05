67a70aa470cacccc264c1fd6eff57ba5518349c3
Jake Edwards <jake.edwards@simplybusiness.com>
2024-06-05 16:56:20 -0400

    add header

--- app/page.tsx
+++ app/page.tsx
@@ -3,35 +3,41 @@ import React from "react";
 
 const Home = () => {
   return (
-    <section className="home md:container md:mx-auto">
-      <div className="home-content">
-
-          <div class="coronavirus-cover-lookup">
-              <div class="module form">
-                  <div class="module-content">
-                      <div class="left-content">
-                          <h2>Estimate in seconds</h2>
-                          <h3>Workers' Comp in a click. Simply slide your payroll tool to get an estimate for your workers' comp
-                              policy!</h3>
-                          <ul>
-                              <li class="credit-card">Slide</li>
-                              <li class="file-certificate">Estimate</li>
-                              <li class="message-smile">Insure</li>
-                          </ul>
-                      </div>
-                      <div class="right-content">
-                          <img alt="Woman and man looking at laptop"
-                               src="woman-and-man-looking-at-laptop.png"></img>
-                      </div>
-                  </div>
+    <>
+      <div className="bg-blue-1 p-20">
+        <div className="md:container md:mx-auto coronavirus-cover-lookup">
+          <div class="module form">
+            <div class="module-content">
+              <div class="left-content">
+                <h2>Estimate in seconds</h2>
+                <h3>
+                  Workers' Comp in a click. Simply slide your payroll tool to
+                  get an estimate for your workers' comp policy!
+                </h3>
+                <ul>
+                  <li class="credit-card">Slide</li>
+                  <li class="file-certificate">Estimate</li>
+                  <li class="message-smile">Insure</li>
+                </ul>
               </div>
+              <div class="right-content">
+                <img
+                  alt="Woman and man looking at laptop"
+                  src="woman-and-man-looking-at-laptop.png"
+                ></img>
+              </div>
+            </div>
           </div>
-
-        <div className="flex flex-col items-center">
-          <PremiumEstimator />
         </div>
       </div>
-    </section>
+      <section className="home md:container md:mx-auto">
+        <div className="home-content">
+          <div className="flex flex-col items-center">
+            <PremiumEstimator />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
